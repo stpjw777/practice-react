54bf8f7ab733449355ed5e49f5bc77c7bb3adf36
jaewan <stpjw777@gmail.com>
2020-01-26 14:37:36 +0900

    yarn start and add styled-components and create button and add function on it.

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Button = styled.a`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  color: pink;
+  border: 2px solid black;
+`;
+
+export default Button;
